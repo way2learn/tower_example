098f7405e24004b074177b4d443d2b61b82d2c5c
way2learn <135120840+way2learn@users.noreply.github.com>
2025-05-19 15:33:15 +0530

    Update testtf1.tf
    plugin version has been updated

--- testtf1.tf
+++ testtf1.tf
@@ -2,7 +2,7 @@ terraform {
    providers {
       onefuse = {
         source = "CloudBoltSoftware/onefuse"
-        version = ">= 1.40.0"
+        version = ">= 1.50.0"
       }
    }
    required_version = ">= 0.13"
